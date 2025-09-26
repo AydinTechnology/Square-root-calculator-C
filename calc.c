f5f6650c4462041604d3da148e7af4bb4a4df6b9
Aydin <159723594+AydinTechnology@users.noreply.github.com>
2025-09-26 23:04:44 +0200

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int terms = 0;
+
+float square_root(float x) {
+        float n0 = 1, n1 = 1;
+        while(1) {
+                terms++;
+
+                n0 = n1;
+                n1 = x/n0 + n0;
+                n1 = n1 / 2;
+
+                if(n1 - n0 == 0) break;
+        }
+        return n1;
+}
+
+int main(int argc, char *argv[]) {
+        float input = atof(argv[1]);
+        printf("Calculating the square root of %f...\n\n", input);
+        printf("The square root of %f is %f\n", input, square_root(input));
+        printf("Converged after %i terms.\n", terms);
+        return 0;
+}
